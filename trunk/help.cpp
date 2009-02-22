540de0d7592ea445545f64929d4d5a2a62ce008e
nknikolov <nknikolov@ba3b9070-f066-11dd-a70c-37889e090ee0>
2009-02-22 11:10:07 +0000

    initial revision

---
+++ trunk/help.cpp
@@ -0,0 +1,176 @@
+/*
+    Copyright (C) 2007 by Nikolay Nikolov <nknikolov@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#define __STDC_CONSTANT_MACROS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "ffmpeg_headers.h"
+#include "utils.h"
+
+extern const tVideoFrameRateAbbr frame_rate_abbrs[];
+extern const tVideoFrameSizeAbbr frame_size_abbrs[];
+
+static void print_abbreviation(void);
+static void print_formats(void);
+static void print_codecs(void);
+
+void print_version(void)
+{
+    printf("%s Version %s\n\n", PACKAGE, VERSION);
+
+    printf("Copyright (C) 2007-2009 Nikolay Nikolov <nknikolov@gmail.com>\n");
+    printf("This program is distributed in the hope that it will be useful,\n"
+           "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+           "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+           "GNU General Public License for more details.\n");
+}
+
+void print_usage(void)
+{
+    printf("Usage: %s [OPTION]... CDGFILE...\n\n", PACKAGE);
+    printf("Try '%s --help' for more options.\n", PACKAGE);
+}
+
+void print_help(void)
+{
+    printf("%s Version %s, convert CDG files to video\n", PACKAGE, VERSION);
+    printf("Usage: %s [OPTION]... CDGFILE...\n", PACKAGE);
+
+    printf("\nGeneral options:\n");
+    printf(" -V  --version              Display the version of %s and exit.\n", PACKAGE);
+    printf(" -h  --help                 Print this help.\n");
+
+    printf("\nEncoding options:\n");
+    printf(" -f  --format   <type>      Specify the output file format (default: avi)\n");
+    
+    printf(" -s             <size>      Set frame size (WxH or abbreviation, default: 352x288)\n");
+    printf("     --aspect   <ratio>     Set aspect ratio (4:3, 16:9 or 1.3333, 1.7777, default: 4:3)\n");
+    
+    printf(" -r             <rate>      Set frame rate (Hz value, fraction or abbreviation, default: pal)\n");
+
+    //printf("\n");
+    //printf("     --acodec   <codec>     Force audio codec\n");
+    //printf("     --vcodec   <codec>     Force video codec\n");
+        
+    printf("\n");
+    printf("     --force-encode-audio   By default, if the input audio codec is the same as the output one,\n");
+    printf("                            the audio is copied. If you specify this option the audio will be\n");
+    printf("                            re-encode always. This can helps in case of corrupted audio files,\n");
+    printf("                            but it's possible to reduce the audio quality.\n");
+
+    print_abbreviation();
+    print_formats();
+    //print_codecs();
+
+    printf("\n");
+}
+
+static void print_abbreviation(void)
+{
+    int i = 0;
+    printf("\nFrame rate abbreviations:\n");
+    
+    while (frame_rate_abbrs[i].abbr) 
+    {
+        printf(" %-15s %.3f fps\n", frame_rate_abbrs[i].abbr, 
+            (double)frame_rate_abbrs[i].rate_num/(double)frame_rate_abbrs[i].rate_den);
+        i++;
+    }
+
+    i = 0;
+    printf("\nFrame size abbreviations:\n");
+
+    while (frame_size_abbrs[i].abbr) 
+    {
+        printf(" %-15s %dx%d\n", frame_size_abbrs[i].abbr, 
+            frame_size_abbrs[i].width, frame_size_abbrs[i].height);
+        i++;
+    }
+}
+
+static void print_formats(void)
+{
+    AVOutputFormat *ofmt;
+    printf("\nFile formats:\n");
+
+#if LIBAVFORMAT_VERSION_INT < ((52<<16)+(2<<8)+0)
+    for (ofmt = first_oformat; ofmt != NULL; ofmt = ofmt->next) 
+#else
+    for (ofmt = av_oformat_next(NULL); ofmt != NULL; ofmt = av_oformat_next(ofmt)) 
+#endif
+    {
+        if (ofmt->name == NULL) break;
+
+        if (ofmt->video_codec == CODEC_ID_NONE) continue;
+        if (ofmt->audio_codec == CODEC_ID_NONE) continue;
+
+        printf(" %-15s %-40s\n", ofmt->name, ofmt->long_name ? ofmt->long_name:" ");
+    }
+}
+
+static void print_codecs(void)
+{
+    AVCodec *codec;
+    int i;
+
+    printf("\nAudio Codecs:\n");
+
+#if LIBAVCODEC_VERSION_INT < ((51<<16)+(49<<8)+0)
+    for(codec = first_avcodec, i = 0; codec != NULL; codec = codec->next) 
+#else
+    for(codec = av_codec_next(NULL), i = 0; codec != NULL; codec = av_codec_next(codec)) 
+#endif
+    {
+        if (codec->name == NULL) break;
+        if (codec->encode == 0) continue;
+        if (codec->type != CODEC_TYPE_AUDIO) continue;
+
+        printf(" %-16s ", codec->name);
+     
+        i++; 
+        if (!(i % 4)) printf("\n");
+    }
+
+    if (i % 4) printf("\n");
+    printf("\nVideo Codecs:\n");
+
+#if LIBAVCODEC_VERSION_INT < ((51<<16)+(49<<8)+0)
+    for(codec = first_avcodec, i = 0; codec != NULL; codec = codec->next) 
+#else
+    for(codec = av_codec_next(NULL), i = 0; codec != NULL; codec = av_codec_next(codec)) 
+#endif
+    {
+        if (codec->name == NULL) break;
+        if (codec->encode == 0) continue;
+        if (codec->type != CODEC_TYPE_VIDEO) continue;
+
+        printf(" %-16s ", codec->name);
+     
+        i++; 
+        if (!(i % 4)) printf("\n");
+    }
+
+    if (i % 4) printf("\n");
+}
---
+++ trunk/utils.cpp
@@ -0,0 +1,189 @@
+/*
+    Copyright (C) 2007 by Nikolay Nikolov <nknikolov@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#define __STDC_CONSTANT_MACROS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
+
+#include "ffmpeg_headers.h"
+#include "utils.h"
+
+#define MAX_FRAME_RATE_BASE 1001000	// maximum allowed frame rate numerator and denominator 
+
+extern const tVideoFrameRateAbbr frame_rate_abbrs[]= 
+{
+    { "ntsc",      30000, 1001 },
+    { "pal",          25,    1 },
+    { "film",         24,    1 },
+    { "ntsc-film", 24000, 1001 },
+    { NULL,            0,    0 }
+};
+
+extern const tVideoFrameSizeAbbr frame_size_abbrs[]= 
+{
+    { "dvd-ntsc",      720, 480 },
+    { "dvd-pal",       720, 576 },
+    { "svcd-ntsc",     480, 480 },
+    { "svcd-pal",      480, 576 },
+    { "vcd-ntsc",      352, 240 },
+    { "vcd-pal",       352, 288 },
+    { NULL,            0,   0   }
+};
+
+static const char* audio_files[] = {
+    "mp3",  "MP3",  "Mp3",
+    "ogg",  "OGG",  "Ogg",
+    "flac", "FLAC", "Flac",
+    NULL
+};
+
+char* get_audio_filename(const char* cdgfile)
+{
+    char* p;
+    char* audiofile = NULL;
+
+    audiofile = (char*)malloc(strlen(cdgfile) + 8);
+    strcpy(audiofile, cdgfile);
+    p = strrchr(audiofile, '.');
+
+    if (p == NULL) {
+        free(audiofile);
+        return NULL;
+    }
+
+    p++;
+
+    for (int i = 0; audio_files[i] != NULL; i++)
+    {
+        strcpy(p, audio_files[i]);
+        if (access(audiofile, R_OK) == 0) return audiofile;
+    }
+
+    free(audiofile);
+    return NULL;
+}
+
+int get_frame_rate(int *frame_rate_num, int *frame_rate_den, const char *arg)
+{
+    int i = 0;
+    char* cp;
+
+    if (frame_rate_num == NULL || frame_rate_den == NULL || arg == NULL)
+        return -1;
+
+    // First, we check our abbreviation table
+    while (frame_rate_abbrs[i].abbr) {
+        if (!strcmp(frame_rate_abbrs[i].abbr, arg)) {
+            *frame_rate_num = frame_rate_abbrs[i].rate_num;
+            *frame_rate_den = frame_rate_abbrs[i].rate_den;
+            return 0;
+        }
+        i++;
+    }
+
+    // Then, we try to parse it as fraction
+    cp = strchr(arg, '/');
+    if (!cp)
+        cp = strchr(arg, ':');
+
+    if (cp) {
+        char* cpp;
+        *frame_rate_num = strtol(arg, &cpp, 10);
+        if (cpp != arg || cpp == cp)
+            *frame_rate_den = strtol(cp+1, &cpp, 10);
+        else
+           *frame_rate_num = 0;
+    }
+    else {
+        // Finally we give up and parse it as double
+        AVRational time_base = av_d2q(strtod(arg, 0), MAX_FRAME_RATE_BASE);
+        *frame_rate_den = time_base.den;
+        *frame_rate_num = time_base.num;
+    }
+
+    if (*frame_rate_num == 0 || *frame_rate_den == 0)
+        return -1;
+    else
+        return 0;
+}
+
+int get_frame_size(int *width_ptr, int *height_ptr, const char *str)
+{
+    int i = 0;
+    const char *p;
+
+    if (width_ptr == NULL || height_ptr == NULL || str == NULL) 
+        return -1;
+
+    while (frame_size_abbrs[i].abbr) {
+        if (!strcmp(frame_size_abbrs[i].abbr, str)) {
+            *width_ptr = frame_size_abbrs[i].width;
+            *height_ptr = frame_size_abbrs[i].height;
+            return 0;
+        }
+        i++;
+    }
+
+    p = str;
+    *width_ptr = strtol(p, (char **)&p, 10);
+
+    if (*p) p++;
+    *height_ptr = strtol(p, (char **)&p, 10);
+
+    if (*width_ptr <= 0 || *height_ptr <= 0)
+        return -1;
+
+    return 0;
+}
+
+int get_aspect_ratio(AVRational *aspect_ratio, const char *arg)
+{
+    char* cp;
+
+    if (aspect_ratio == NULL || arg == NULL)
+        return -1;
+
+    // Try to parse it as fraction
+    cp = strchr(arg, '/');
+    if (!cp)
+        cp = strchr(arg, ':');
+
+    if (cp) {
+        char* cpp;
+        aspect_ratio->num = strtol(arg, &cpp, 10);
+        if (cpp != arg || cpp == cp)
+            aspect_ratio->den = strtol(cp+1, &cpp, 10);
+        else
+           aspect_ratio->num = 0;
+    }
+    else {
+        // Finally we give up and parse it as double
+        *aspect_ratio = av_d2q(strtod(arg, 0), MAX_FRAME_RATE_BASE);
+    }
+
+    if (aspect_ratio->num == 0 || aspect_ratio->den == 0)
+        return -1;
+    else
+        return 0;
+}
