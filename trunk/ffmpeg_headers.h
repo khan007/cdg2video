0d84715c81ddb9cdf25490dcadba973aedef1dd1
nknikolov@gmail.com <nknikolov@gmail.com@ba3b9070-f066-11dd-a70c-37889e090ee0>
2014-08-16 17:05:09 +0000

    use avcodec_encode_video2

--- trunk/ffmpeg_headers.h
+++ trunk/ffmpeg_headers.h
@@ -3,19 +3,14 @@
 #endif
 
 extern "C" {
-#ifdef OLD_FFMPEG_INCLUDES
-  #include "ffmpeg/avcodec.h"
-  #include "ffmpeg/avformat.h"
-  #include "ffmpeg/swscale.h"
-  #include "ffmpeg/fifo.h"
-#else
   #include "libavcodec/avcodec.h"
   #include "libavformat/avformat.h"
   #include "libswscale/swscale.h"
   #include "libavutil/fifo.h"
   #include "libavutil/mathematics.h"
   #include "libavutil/channel_layout.h"
-#endif
+  #include "libavutil/frame.h"
+  #include "libavutil/imgutils.h"
 }
 
 
